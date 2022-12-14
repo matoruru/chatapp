472805352afae15cb0be849ca9aa87f8df63d309
matoruru <matoruru@shgt.jp>
2022-12-14 23:27:24 +0900

    Updated

--- index.ts
+++ index.ts
@@ -10,21 +10,23 @@ const server = http.createServer(app)
 const io = new Server(server)
 
 const createToken = (id: string) => {
-  console.log(`Token generated with id ${id}`)
   const str = `fjewoipafjapewh${id}`
   return crypto.createHash("SHA1").update(str).digest('hex')
 }
 
-io.on('connection', (socket) => {
-  console.log('a user connected')
-  const token = createToken(socket.id)
-  io.to(socket.id).emit("token", { token })
+const appLog = (s: string) => {
+  console.log(`[${new Date().toLocaleString('ja-JP')}] ${s}`)
+}
 
+io.on('connection', (socket) => {
+  const token = createToken(socket.id)
+  appLog(`A user "${token}" connected.`)
+  io.to(socket.id).emit("token", { token })
   socket.on('disconnect', function () {
-    console.log('a user disconnected');
+    appLog(`A user "${token}" disconnected.`)
   });
   socket.on('sendMessage', (message) => {
-    console.log(message)
+    appLog(message)
     io.emit('receiveMessage', message)
   })
 })
@@ -32,5 +34,5 @@ io.on('connection', (socket) => {
 app.use('/', express.static(path.join(__dirname, '/frontend/build')))
 
 server.listen(port, () => {
-  console.log(`Chat app listening on port ${port}`)
+  appLog(`Chat app listening on port ${port}`)
 })
